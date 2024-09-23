ba4c89a12f5d3602878a82d333444428a97af174
Horacio935 <lopezjosecbsl@gmail.com>
2024-09-23 11:17:09 -0600

    prubea2.0

--- app/config/env.js
+++ app/config/env.js
@@ -4,7 +4,7 @@ const env = {
   database: 'prueba_8vxu',
   username: 'prueba',
   password: 'yf7EzAphnX53681TqmYY2HENgBWnUHtP',
-  host: 'dpg-cropfce8ii6s73bf48pg-a.oregon-postgres.render.com',
+  host: 'dpg-cropfce8ii6s73bf48pg-a',
   dialect: 'postgres',
   pool: {
     max: 5,
